391e60b37e9ef03ff8d5a6f470df5fd3186eb511
krrish-kohli <krrishkohli@icloud.com>
2024-08-24 17:12:01 -0700

    added

---
+++ birthday-wisher-start/main.py
@@ -0,0 +1,38 @@
+import smtplib
+import datetime as dt
+import pandas
+import random
+
+MY_EMAIL = "pythoncodetester4@gmail.com"
+PASSWORD = "zaaizqkyqbbloetb"
+
+# Check if today matches a birthday in the birthdays.csv
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+# If it is true, pick a random letter from letter templates
+# and replace the [NAME] with the person's actual name from birthdays.csv
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+# Send the letter generated in step 3 to that person's email address.
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday\n\n{contents}"
+        )
+
+
+
+
+
